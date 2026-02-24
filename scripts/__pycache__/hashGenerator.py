b9ba28031da427489343110b6846e3d4105f408e
Daniel <danielcaron1995@gmail.com>
2026-02-23 19:27:41 -0800

    Created the Hash Generator Class

--- scripts/__pycache__/hashGenerator.py
+++ scripts/__pycache__/hashGenerator.py
@@ -1,52 +1,100 @@
-class HashGenerator:
-
-    def __init__(self):
-        pass
-
-    def encrypt():
-        pass
-
-    def decrypt():
-        pass
-
-
 import os
 from cryptography.hazmat.primitives.kdf.argon2 import Argon2id
-salt = os.urandom(16)
-# derive
-kdf = Argon2id(
-    salt=salt,
-    length=32,
-    iterations=1,
-    lanes=4,
-    memory_cost=64 * 1024,
-    ad=None,
-    secret=None,
-)
-
-#This function generates a ket using KDF based on the input password
-
 import base64
 import os
 from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-password = b"password"
-salt = os.urandom(16)
-kdf = PBKDF2HMAC(
-    algorithm=hashes.SHA256(),
-    length=32,
-    salt=salt,
-    iterations=1_200_000,
-)
-key = base64.urlsafe_b64encode(kdf.derive(password))
-print(key)
 
-f = Fernet(key)
-print(f)
 
-token = f.encrypt(b"Secret message!")
-print(token)
+class HashGenerator:
+    '''
+    This class provides the neccessary methods to perform various 
+    functions for encrypting passwords and generating encryption keys.
+    '''
 
-b'...'
-print(f.decrypt(token))
+
+    def generateHash(self, message : str | bytes, key):
+        '''
+        This method is used for when adding new passwords that 
+        need to be encrypted
+        '''
+        if type(message) == str:
+            message = message.encode("UTF-8")
+
+        f = Fernet(key)
+
+        eencryptedMessage = f.encrypt(message)
+
+        return eencryptedMessage
+
+
+    def decryptHash(self, eencryptedMessage : bytes, key):
+
+        f = Fernet(key)
+
+        decryptedMessage =f.decrypt(eencryptedMessage)
+        decryptedMessage = decryptedMessage.decode()
+
+        return decryptedMessage
+
+
+    def generateKey(self, eneredPassword, salt = 0):
+        '''
+        This is used to generate the encrryption key
+        from the User's master password, the first time it is run,
+        The salt will be generated and can be stored later.
+        '''
+
+        passwordBytes = eneredPassword.encode("utf-8")
+
+        if salt == 0:
+            salt = self.generateSalt()
+
+        kdf = PBKDF2HMAC(
+            algorithm=hashes.SHA256(),
+            length=32,
+            salt=salt,
+            iterations=1_200_000,
+        )
+
+        key = base64.urlsafe_b64encode(kdf.derive(passwordBytes))
+
+        return key, salt
+    
+
+    def generateSalt(self):
+
+        salt = os.urandom(16)
+
+        return salt
+
+
+
+hg = HashGenerator()
+
+Masterpassword = "Apple"
+
+keySalt  = hg.generateKey(Masterpassword)
+key = keySalt[0]
+salt = keySalt[1]
+
+newPassword = 'Akmeer123@'
+
+
+encrypedPasswordHash = hg.generateHash(newPassword, key)
+print(encrypedPasswordHash)
+
+
+decryptedPassword = hg.decryptHash(encrypedPasswordHash, key)
+print(decryptedPassword)
+
+
+
+
+
+
+
+
+
+
